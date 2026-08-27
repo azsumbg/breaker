df4807aafc1f8d6a8556f0466d44f02b30adf7bb
Daniel Dinev <azsumbg@gmail.com>
2026-08-27 09:41:11 +0300

    Add project files.

---
+++ breaker/breaker.cpp
@@ -0,0 +1,428 @@
+﻿#include "pch.h"
+#include "breaker.h"
+
+// EXCEPTION CLASS ************************
+
+dll::EXCEPTION::EXCEPTION(int err_code)
+{
+	error = err_code;
+}
+
+const wchar_t* dll::EXCEPTION::getErr()const
+{
+	switch (error)
+	{
+	case BAD_INDEX:
+		return L"Bad index passed to a BAG method !";
+
+	case BAD_PARAM:
+		return L"Bad parameter passed to a BAG method !";
+
+	case BAD_PTR:
+		return L"Nullptr pointer passed to a BAG method !";
+
+	case BAD_MEMORY:
+		return L"Bad memory allocation in realloc() !";
+	}
+
+	return L"Unknown error occurred in a BAG method !";
+}
+
+///////////////////////////////////////////
+
+// RANDIT CLASS ****************************
+
+dll::RANDIT::RANDIT()
+{
+	std::random_device rd{};
+	std::seed_seq sq{ rd(),rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+	twister = new std::mt19937(sq);
+}
+dll::RANDIT::~RANDIT()
+{
+	if (twister)delete twister;
+}
+
+int dll::RANDIT::operator()(int min, int max)
+{
+	if (max < min)return 0;
+
+	std::uniform_int_distribution distrib(min, max);
+
+	return distrib(*twister);
+}
+float dll::RANDIT::operator()(float min, float max)
+{
+	if (max < min)return 0;
+
+	std::uniform_real_distribution<float> distrib(min, max);
+
+	return distrib(*twister);
+}
+
+////////////////////////////////////////////
+
+// PROTON CLASS ***************************
+
+dll::PROTON::PROTON()
+{
+	start.x = 0;
+	start.y = 0;
+
+	_width = 1.0f;
+	_height = 1.0f;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	myRect.left = start.x;
+	myRect.top = start.y;
+	myRect.right = end.x;
+	myRect.bottom = end.y;
+}
+dll::PROTON::PROTON(float first_x, float first_y)
+{
+	start.x = first_x;
+	start.y = first_y;
+
+	_width = 1.0f;
+	_height = 1.0f;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	myRect.left = start.x;
+	myRect.top = start.y;
+	myRect.right = end.x;
+	myRect.bottom = end.y;
+}
+dll::PROTON::PROTON(float first_x, float first_y, float first_width, float first_height)
+{
+	start.x = first_x;
+	start.y = first_y;
+
+	_width = first_width;
+	_height = first_height;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	myRect.left = start.x;
+	myRect.top = start.y;
+	myRect.right = end.x;
+	myRect.bottom = end.y;
+}
+
+void dll::PROTON::set_edges()
+{
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	myRect.left = start.x;
+	myRect.top = start.y;
+	myRect.right = end.x;
+	myRect.bottom = end.y;
+}
+void dll::PROTON::set_width(float new_width)
+{
+	_width = new_width;
+	
+	x_rad = _width / 2.0f;
+	
+	center.x = start.x + x_rad;
+	
+	end.x = start.x + _width;
+	
+	myRect.left = start.x;
+	myRect.right = end.x;
+}
+void dll::PROTON::set_height(float new_height)
+{
+	_height = new_height;
+
+	y_rad = _height / 2.0f;
+
+	center.y = start.y + y_rad;
+
+	end.y = start.y + _height;
+
+	myRect.top = start.y;
+	myRect.bottom = end.y;
+}
+void dll::PROTON::set_dims(float new_width, float new_height)
+{
+	_width = new_width;
+	_height = new_height;
+
+	x_rad = _width / 2.0f;
+	y_rad = _height / 2.0f;
+
+	center.x = start.x + x_rad;
+	center.y = start.y + y_rad;
+
+	end.x = start.x + _width;
+	end.y = start.y + _height;
+
+	myRect.left = start.x;
+	myRect.top = start.y;
+	myRect.right = end.x;
+	myRect.bottom = end.y;
+}
+
+float dll::PROTON::get_width() const
+{
+	return _width;
+}
+float dll::PROTON::get_height() const
+{
+	return _height;
+}
+D2D1_RECT_F dll::PROTON::get_rect()const
+{
+	return myRect;
+}
+
+bool dll::PROTON::created_in_heap()const
+{
+	return in_heap;
+}
+
+dll::PROTON* dll::PROTON::create(float first_x, float first_y, float first_width, float first_height)
+{
+	PROTON* ret{ new PROTON(first_x, first_y, first_width, first_height) };
+
+	ret->in_heap = true;
+
+	return ret;
+}
+
+///////////////////////////////////////////
+
+// GRID CLASS ****************************
+
+dll::GRID::GRID(int number_of_bricks)
+{
+	Bricks.clear();
+
+	int brick_counter = 0;
+	int stone_bricks = number_of_bricks / 10;
+
+	float brick_x = 5.0f;
+	float brick_y = 60.0f;
+
+	while (brick_counter < number_of_bricks)
+	{
+		for (int cols = 0; cols < 15; ++cols)
+		{
+			BRICK dummy{};
+			
+			dummy.rect.left = brick_x;
+			dummy.rect.top = brick_y;
+			dummy.rect.right = brick_x + 50.0f;
+			dummy.rect.bottom = brick_y + 20.0f;
+
+			dummy.type = static_cast<bricks>(_randerer(0, 4));
+			dummy.number = brick_counter;
+
+			switch (dummy.type)
+			{
+			case bricks::yellow:
+				dummy.lifes = 1;
+				break;
+
+			case bricks::red:
+				dummy.lifes = 2;
+				break;
+
+			case bricks::blue:
+				dummy.lifes = 4;
+				break;
+
+			case bricks::green:
+				dummy.lifes = 3;
+				break;
+			}
+			dummy.value = dummy.lifes;
+
+			Bricks.push_back(dummy);
+
+			brick_x += 52.5f;
+
+			++brick_counter;
+		}
+
+		brick_x = 5.0f;
+		brick_y += 22.5f;
+	}
+
+	for (int i = 0; i < stone_bricks; ++i)
+	{
+		bool ok = false;
+
+		while (!ok)
+		{
+			ok = true;
+
+			int stone_number = _randerer(0, brick_counter - 1);
+			if (Bricks[stone_number].type != bricks::stone)Bricks[stone_number].type = bricks::stone;
+			else ok = false;
+		}
+	}
+
+	bricks_in_grid = number_of_bricks;
+	bricks_to_crush = bricks_in_grid - stone_bricks;
+}
+
+void dll::GRID::create_grid(int all_bricks)
+{
+	Bricks.clear();
+
+	int brick_counter = 0;
+	int stone_bricks = all_bricks / 10;
+
+	float brick_x = 5.0f;
+	float brick_y = 60.0f;
+
+	while (brick_counter < all_bricks)
+	{
+		for (int cols = 0; cols < 15; ++cols)
+		{
+			BRICK dummy{};
+
+			dummy.rect.left = brick_x;
+			dummy.rect.top = brick_y;
+			dummy.rect.right = brick_x + 50.0f;
+			dummy.rect.bottom = brick_y + 20.0f;
+
+			dummy.type = static_cast<bricks>(_randerer(0, 4));
+			dummy.number = brick_counter;
+
+			switch (dummy.type)
+			{
+			case bricks::yellow:
+				dummy.lifes = 1;
+				break;
+
+			case bricks::red:
+				dummy.lifes = 2;
+				break;
+
+			case bricks::blue:
+				dummy.lifes = 4;
+				break;
+
+			case bricks::green:
+				dummy.lifes = 3;
+				break;
+			}
+			dummy.value = dummy.lifes;
+
+			Bricks.push_back(dummy);
+
+			brick_x += 52.5f;
+
+			++brick_counter;
+		}
+
+		brick_x = 5.0f;
+		brick_y += 22.5f;
+	}
+
+	for (int i = 0; i < stone_bricks; ++i)
+	{
+		bool ok = false;
+
+		while (!ok)
+		{
+			ok = true;
+
+			int stone_number = _randerer(0, brick_counter - 1);
+			if (Bricks[stone_number].type != bricks::stone)Bricks[stone_number].type = bricks::stone;
+			else ok = false;
+		}
+	}
+
+	bricks_in_grid = all_bricks;
+	bricks_to_crush = bricks_in_grid - stone_bricks;
+}
+
+BRICK dll::GRID::get_brick(int number)
+{
+	BRICK ret{};
+
+	if (number < 0 || number >= Bricks.size())return ret;
+
+	ret = Bricks[number];
+
+	return ret;
+}
+BRICK dll::GRID::get_brick(D2D1_RECT_F brick_rect)
+{
+	BRICK ret{};
+
+	ret.number = -1;
+
+	for (int i = 0; i < Bricks.size(); ++i)
+	{
+		D2D1_RECT_F dummy = Bricks[i].rect;
+
+		if (dummy.left == brick_rect.left && dummy.right == brick_rect.right
+			&& dummy.top == brick_rect.top && dummy.bottom == brick_rect.bottom)
+		{
+			ret = Bricks[i];
+			break;
+		}
+	}
+
+	return ret;
+}
+
+void dll::GRID::remove_brick(int number)
+{
+	if (number < 0 || number >= Bricks.size())return;
+
+	Bricks.erase(number);
+}
+void dll::GRID::remove_brick(D2D1_RECT_F brick_rect)
+{
+	BRICK ret{};
+
+	ret.number = -1;
+
+	for (int i = 0; i < Bricks.size(); ++i)
+	{
+		D2D1_RECT_F dummy = Bricks[i].rect;
+
+		if (dummy.left == brick_rect.left && dummy.right == brick_rect.right
+			&& dummy.top == brick_rect.top && dummy.bottom == brick_rect.bottom)
+		{
+			ret = Bricks[i];
+			break;
+		}
+	}
+
+	if (ret.number != -1)Bricks.erase(ret.number);
+}
+
+//////////////////////////////////////////
---
+++ breaker/breaker.h
@@ -0,0 +1,486 @@
+﻿#pragma once
+
+#ifdef BREAKER_EXPORTS
+#define BREAKER_API __declspec(dllexport)
+#else
+#define BREAKER_API __declspec(dllimport)
+#endif
+
+#include <d2d1.h>
+#include <random>
+
+#pragma comment(lib, "d2d1.lib")
+
+constexpr float scr_width{ 1000.0f };
+constexpr float scr_height{ 800.0f };
+
+constexpr float sky{ 50.0f };
+constexpr float ground{ 750.0f };
+
+constexpr D2D1_RECT_F FULL_SCREEN{ 0, 0, 1000.0f, 800.0f };
+
+constexpr int BAD_PTR{ 6001 };
+constexpr int BAD_PARAM{ 6002 };
+constexpr int BAD_INDEX{ 6003 };
+constexpr int BAD_MEMORY{ 6004 };
+
+enum class bricks { yellow = 0, blue = 1, red = 2, green = 3, orange = 4, stone = 5 };
+enum class assets { triple = 0, life = 1, shorten = 2, strech = 3 };
+
+struct BRICK
+{
+	bricks type{ bricks::yellow };
+
+	D2D1_RECT_F rect;
+
+	int number = 0;
+	int lifes = 1;
+	int value = 1;
+};
+
+namespace dll
+{
+	class BREAKER_API RANDIT
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIT();
+		~RANDIT();
+
+		int operator()(int min, int max);
+		float operator()(float min, float max);
+	};
+
+	class BREAKER_API EXCEPTION
+	{
+	private:
+		int error{ 0 };
+
+	public:
+		EXCEPTION(int err_code);
+
+		const wchar_t* getErr()const;
+	};
+
+	template<typename T> class BAG
+	{
+	private:
+		size_t max_size{ 1 };
+		size_t next_pos{ 0 };
+		T* mPtr{ nullptr };
+
+	public:
+		BAG()
+		{
+			mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+		}
+		BAG(size_t capacity)
+		{
+			max_size = capacity;
+			mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+		}
+		BAG(BAG& other)
+		{
+			if (other.mPtr == nullptr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				if (mPtr == nullptr)throw EXCEPTION(BAD_PTR);
+				else
+				{
+					if (next_pos > 0)for (size_t i = 0; i < next_pos; ++i)mPtr[i] = other.mPtr[i];
+				}
+			}
+		}
+		BAG(BAG&& other)
+		{
+			if (other.mPtr == nullptr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = other.mPtr;
+				
+				other.mPtr = nullptr;
+			}
+		}
+
+		~BAG()
+		{
+			free(mPtr);
+		}
+
+		bool empty()const
+		{
+			return(next_pos == 0);
+		}
+		size_t size()const
+		{
+			return next_pos;
+		}
+		size_t capacity()const
+		{
+			return max_size;
+		}
+
+		BAG& operator=(BAG& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				free(mPtr);
+
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				if (mPtr == nullptr)throw EXCEPTION(BAD_PTR);
+				else
+				{
+					if (next_pos > 0)for (size_t i = 0; i < next_pos; ++i)mPtr[i] = other.mPtr[i];
+				}
+
+			}
+
+			return *this;
+		}
+		BAG& operator=(BAG&& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				free(mPtr);
+
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = other.mPtr;
+
+				other.mPtr = nullptr;
+			}
+
+			return *this;
+		}
+
+		T& operator[](size_t index)
+		{
+			if (index < 0 || index >= next_pos)throw EXCEPTION(BAD_INDEX);
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+
+			return mPtr[index];
+		}
+
+		T& front()
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			if (next_pos == 0)throw EXCEPTION(BAD_INDEX);
+
+			return *mPtr;
+		}
+		T& back()
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			if (next_pos == 0)throw EXCEPTION(BAD_INDEX);
+
+			return mPtr[next_pos-1];
+		}
+
+		void clear()
+		{
+			free(mPtr);
+			
+			max_size = 1;
+			next_pos = 0;
+
+			mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+		}
+
+		void push_back(T element)
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					mPtr[next_pos] = element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+						mPtr[next_pos] = element;
+						++next_pos;
+					}
+				}
+			}
+		}
+		void push_back(T* element)
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					mPtr[next_pos] = *element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+						mPtr[next_pos] = *element;
+						++next_pos;
+					}
+				}
+			}
+		}
+
+		void push_front(T element)
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					*mPtr = element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+						*mPtr = element;
+						++next_pos;
+					}
+				}
+			}
+		}
+		void push_front(T* element)
+		{
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					*mPtr = *element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+						*mPtr = *element;
+						++next_pos;
+					}
+				}
+			}
+		}
+		
+		void erase(size_t index)
+		{
+			if (index < 0 || index >= next_pos)throw EXCEPTION(BAD_INDEX);
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				for (size_t i = index; i < next_pos - 1; ++i)mPtr[i] = mPtr[i + 1];
+
+				--max_size;
+
+				T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+				if (!dummy)throw EXCEPTION(BAD_MEMORY);
+				else
+				{
+					mPtr = dummy;
+					dummy = nullptr;
+				}
+
+				--next_pos;
+			}
+		}
+
+		void insert(T element, size_t index)
+		{
+			if (index < 0 || index > next_pos)throw EXCEPTION(BAD_INDEX);
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					if (index == next_pos)
+					{
+						mPtr[next_pos] = element;
+						++next_pos;
+					}
+					else
+					{
+						for (size_t i = next_pos; i > index; --i)mPtr[i] = mPtr[i - 1];
+						mPtr[index] = element;
+						++next_pos;
+					}
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+						
+						if (index == next_pos)
+						{
+							mPtr[next_pos] = element;
+							++next_pos;
+						}
+						else
+						{
+							for (size_t i = next_pos; i > index; --i)mPtr[i] = mPtr[i - 1];
+							mPtr[index] = element;
+							++next_pos;
+						}
+					}
+				}
+			}
+		}
+		void insert(T* element, size_t index)
+		{
+			if (index < 0 || index > next_pos)throw EXCEPTION(BAD_INDEX);
+			if (!mPtr)throw EXCEPTION(BAD_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					if (index == next_pos)
+					{
+						mPtr[next_pos] = *element;
+						++next_pos;
+					}
+					else
+					{
+						for (size_t i = next_pos; i > index; --i)mPtr[i] = mPtr[i - 1];
+						mPtr[index] = *element;
+						++next_pos;
+					}
+				}
+				else
+				{
+					++max_size;
+
+					T* dummy = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!dummy)throw EXCEPTION(BAD_MEMORY);
+					else
+					{
+						mPtr = dummy;
+						dummy = nullptr;
+
+						if (index == next_pos)
+						{
+							mPtr[next_pos] = *element;
+							++next_pos;
+						}
+						else
+						{
+							for (size_t i = next_pos; i > index; --i)mPtr[i] = mPtr[i - 1];
+							mPtr[index] = *element;
+							++next_pos;
+						}
+					}
+				}
+			}
+		}
+	};
+
+
+	///////////////////////////////////////////
+
+	class BREAKER_API PROTON
+	{
+	protected:
+		float _width{ 0 };
+		float _height{ 0 };
+		D2D1_RECT_F myRect{};
+
+		bool in_heap{ false };
+
+	public:
+		D2D1_POINT_2F start{};
+		D2D1_POINT_2F end{};
+		D2D1_POINT_2F center{};
+
+		float x_rad{ 0 };
+		float y_rad{ 0 };
+		
+		PROTON();
+		PROTON(float first_x, float first_y);
+		PROTON(float first_x, float first_y, float first_width, float first_height);
+
+		virtual ~PROTON() {};
+
+		void set_edges();
+		void set_width(float new_width);
+		void set_height(float new_height);
+		void set_dims(float new_width, float new_height);
+
+		float get_width() const;
+		float get_height() const;
+		D2D1_RECT_F get_rect()const;
+
+		bool created_in_heap()const;
+
+		static PROTON* create(float first_x, float first_y, float first_width, float first_height);
+	};
+
+	class BREAKER_API GRID
+	{
+	private:
+		BAG<BRICK> Bricks{};
+		RANDIT _randerer{};
+
+	public:
+		int bricks_in_grid{ 0 };
+		int bricks_to_crush{ 0 };
+
+		GRID(int number_of_bricks);
+
+		void create_grid(int all_bricks);
+
+		BRICK get_brick(int number);
+		BRICK get_brick(D2D1_RECT_F brick_rect);
+
+		void remove_brick(int number);
+		void remove_brick(D2D1_RECT_F brick_rect);
+	};
+
+
+}
